d66ee34ba6a102b00b35afed258acb9aa560f4a3
Daniel Pelaez <estigma88@gmail.com>
2020-02-07 09:15:06 -0500

    Adding operations

--- src/main/java/org/medellin/app/calculator/BasicCalculator.java
+++ src/main/java/org/medellin/app/calculator/BasicCalculator.java
@@ -12,4 +12,21 @@ public class BasicCalculator {
 		return number1 + number2;
 	}
 
+	public Long div(Long number1, Long number2) {
+		logger.info( "Div {} + {}", number1, number2 );
+		if(number2 == 0){
+			throw new IllegalArgumentException("Div by zero cannot be done");
+		}
+		return number1 / number2;
+	}
+
+	public Long subs(Long number1, Long number2) {
+		logger.info( "Substract {} + {}", number1, number2 );
+		return number1 - number2;
+	}
+
+	public Long mul(Long number1, Long number2) {
+		logger.info( "Multiply {} + {}", number1, number2 );
+		return number1 * number2;
+	}
 }
